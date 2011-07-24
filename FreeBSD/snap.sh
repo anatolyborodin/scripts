f3385e07ec1b813f879296da5b0c70480516e67c
Anatoly Borodin <anatoly.borodin@gmail.com>
2011-07-24 14:22:56 +0200

    Use -k with apg, check the passwords with cracklib

--- FreeBSD/snap.sh
+++ FreeBSD/snap.sh
@@ -3,3 +3,4 @@
 TIMESTAMP="`date -u +%Y_%m_%d_%H_%M_%S`"
 
 mksnap_ffs "/.snap/${TIMESTAMP}"
+mksnap_ffs "/.oldroot/.snap/${TIMESTAMP}"
--- passgen.sh
+++ passgen.sh
@@ -5,6 +5,6 @@ LENGTHS="8 10 12"
 
 for l in ${LENGTHS}; do
 	echo "${l} character(s)"
-	apg -M SNCL -n ${COUNT} -m ${l} -x ${l}
+	apg -k -M SNCL -n ${COUNT} -m ${l} -x ${l}
 	echo
 done
